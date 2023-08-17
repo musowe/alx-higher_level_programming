bdef45e227fde37ecf69a276d6315c5c5b07a7bf
musowe <envoymalumbemusowe@gmail.com>
2023-08-17 15:33:56 -0700

    MySQL tasks

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- A script that Lists privileges for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- This script Lists privileges for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- An alx project script that creates the table unique_id on your MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
